81e55d4f9b334b04bc31d08b382e27c56954452a
Edwin Pozo <1391503043+Pozo15@users.noreply.github.com>
2025-08-01 02:09:54 -0400

    Edición de Indices

--- script.js
+++ script.js
@@ -50,3 +50,30 @@ function eliminar(index) {
 }
 
 renderTabla();
+
+
+
+function editarUsuario(index) {
+    const lista = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const usuario = lista[index];
+  
+    document.getElementById("nombre").value = usuario.nombre;
+  
+    document.getElementById("form").onsubmit = function(e) {
+      e.preventDefault();
+  
+      const nuevoNombre = document.getElementById("nombre").value.trim();
+      if (!nuevoNombre || nuevoNombre.length < 2) {
+        alert("El nombre editado no es válido");
+        return;
+      }
+  
+      usuario.nombre = nuevoNombre;
+      lista[index] = usuario;
+      localStorage.setItem("usuarios", JSON.stringify(lista));
+  
+      document.getElementById("form").reset();
+      document.getElementById("form").onsubmit = null;
+      mostrarUsuarios();
+    };
+  }
